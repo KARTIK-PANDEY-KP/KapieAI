e0da24d293dcd9aa5c2edcf9afe231d3f8c73815
KARTIK PANDEY <kartikpandey294@gmail.com>
2021-09-17 13:40:29 +0530

    see

---
+++ index.js
@@ -0,0 +1,11 @@
+var a = true;
+$(".burger").on("click",function(){
+  if(a){
+    $(".nav-links").addClass("pressed");
+    a = false;
+  }else{
+    $(".nav-links").removeClass("pressed");
+    a = true;
+  }
+
+})
